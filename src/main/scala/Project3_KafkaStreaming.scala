dfbc13b85c56f88ff8d216992d286f5e0a4d468c
anassismaili <anassismaili96@gmail.com>
2022-02-23 14:42:54 -0600

    Update

--- src/main/scala/Project3_KafkaStreaming.scala
+++ src/main/scala/Project3_KafkaStreaming.scala
@@ -1,9 +1,25 @@
+import java.sql.DriverManager
+import java.sql.Connection
 
+object QauntityType {
+  def main(args: Array[String]): Unit = {
+      // connect to the database named "mysql" on the localhost
+      val driver = "com.mysql.jdbc.Driver"
+      val url = "jdbc:mysql://localhost:3306/project3" // Modify for whatever port you are running your DB on
+      val username = "root"
+      val password = "monalisa.123" // Update to include your password
+      var connection:Connection = null
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
 
-object Project3_KafkaStreaming {
-    def main(args: Array[String]): Unit={
+      val statement = connection.createStatement()
+      var qt = new QuntityTypeOr()
 
-        // main body of code
-    }
-  
+      qt.payTy()
+      qt.quantityOrder()
+      println(qt.quantityOrder() + ", " + qt.payTy())
+      var query= "INSERT INTO payTyQuOr (pyament_type, quantity_order) Values (" + "'" + qt.payTy().toString + "', " + qt.quantityOrder() +");"
+      print(query)
+      statement.executeUpdate(query)
+  }
 }

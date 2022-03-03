356225b20b43d232bd0c6bfce65922cac046b180
Nick <nicholas@flinn.net>
2022-03-03 11:02:21 -0500

    Added ProductID and CustomerName classes.

---
+++ ProductID.scala
@@ -0,0 +1,15 @@
+class ProductID {
+    private var id = 0
+
+    def getNum(): Int = {
+        var randomNum = scala.util.Random
+        id = randomNum.nextInt(15000)
+        return id
+    }
+
+// if product = "a", then product starts with 1
+// if product = "b", then product starts with 2
+// how many place holders? 100 items, 25 categories?
+// 
+
+}
